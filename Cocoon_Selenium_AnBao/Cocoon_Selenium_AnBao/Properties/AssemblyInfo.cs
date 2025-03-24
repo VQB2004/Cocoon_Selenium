85c6ac110ebb23cdd4cc7465ee805a8e35832b5b
QuocBao <baovo32911@gmail.com>
2025-03-24 22:20:19 +0700

    TestLogin && TestSearch

---
+++ Cocoon_Selenium_AnBao/Cocoon_Selenium_AnBao/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cocoon_Selenium_AnBao")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cocoon_Selenium_AnBao")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e2515b18-4be8-48cc-9269-ce0004f08cdb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cocoon_Selenium_AnBao/Cocoon_Selenium_AnBao/TestLogin.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Cocoon_Selenium_AnBao
+{
+    [TestClass]
+    public class TestLogin
+    {
+        [TestMethod]
+        public void Test_Login_AnBao()
+        {
+            // SDT: 0326929359
+            // pass: Bao12345@
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://cocoonvietnam.com/");
+
+            IWebElement login = driver.FindElement(By.CssSelector("#navbar > div.hidden.lg" +
+                "\\:block > nav > div.main-menu__right > div > button:nth-child(1)"));
+            login.Click();
+
+            IWebElement tel = driver.FindElement(By.CssSelector("#__layout > div > header > div:nth-child(5)" +
+                " > div > div > form > div.phone-input > div > input"));
+            tel.SendKeys("0326929359");
+
+            IWebElement password = driver.FindElement(By.CssSelector("#__layout > div > header > div:nth-child(5)" +
+                " > div > div > form > div:nth-child(5) > div > input"));
+            password.SendKeys("Bao12345@");
+
+            IWebElement button = driver.FindElement(By.CssSelector("#__layout > div > header > div:nth-child(5) > " +
+                "div > div > form > button"));
+            Thread.Sleep(1000); // quan sát dữ liệu được điền
+
+            button.Click();
+
+            Thread.Sleep(1000); // chờ để bắt element bên dưới
+            bool isLogin;
+            string err = "Đăng nhập không thành công";
+
+            try
+            {
+                // nếu không bắt được account => ném ngoại lệ đăng nhập không thành công
+                IWebElement accont = driver.FindElement(By.CssSelector("#navbar > div.hidden.lg" +
+                    "\\:block > nav > div.main-menu__right > div > div > div"));
+                isLogin = true;
+
+            }
+            catch (NoSuchElementException)
+            {
+
+                isLogin = false;
+            }
+            Assert.IsTrue(isLogin, err);
+        }
+
+
+
+    }
+}
+
